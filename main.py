ea0d738e4508081ef7a96e2b2b451ae8e3cb2342
pi55man <cuinnv@gmail.com>
2024-10-14 14:01:34 +0530

    basic class & import pil

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
 import tkinter as tk
+from PIL import ImageTk, Image
+from tkinter import filedialog
 
-app = tk.Tk()
 
-app.title("project")
-app.geometry("800x600")
+class App():
+    def __init__(self, title="photo editor", color="gray"):
+        self.app = tk.Tk()
+        self.app.title(title)
+        self.app.geometry("800x600")
+        self.app.resizable(width=True, height=True)
+        self.app.configure(background=color)
 
-app.configure(background="#3b3b3b")
-app.mainloop()
+    def keybind(self, key, operation):
+        self.app.bind(key, operation)
+
+    def run(self):
+        self.app.mainloop()
+
+    def close(self):
+        self.app.destroy()
+
+
+app = App()
+# close app by pressing escape
+app.keybind("<Escape>", app.close())
+
+
+
+app.run()
